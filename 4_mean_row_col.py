19a22ab692c79c1b9f32815ac0e30b8a2e1f1dba
Arnava Soni <arnavasoni1@gmail.com>
2024-10-13 20:45:35 +0530

    Latest Commit

---
+++ 4_mean_row_col.py
@@ -0,0 +1,16 @@
+import numpy as np
+def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
+    matrix = np.array(matrix)
+    res = []
+    if mode == 'column':
+        res = np.mean(matrix, axis = 0)
+    
+    if mode == 'row':
+        res = np.mean(matrix, axis = 1)
+    
+    return res.tolist()
+
+matrix = [[1, 3, 5],
+          [2, 4, 6]]
+
+print(calculate_matrix_mean(matrix, "column"))
